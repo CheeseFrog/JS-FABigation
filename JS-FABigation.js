b281e5ee0f39effceacbcffba404479fa0a7403d
CheeseFrog <au.yousef+dev@gmail.com>
2021-07-05 00:29:32 +1000

    Add files via upload

--- JS-FABigation.js
+++ JS-FABigation.js
@@ -1,44 +1,84 @@
+// FABigation 0.92
 // Add this : <script src="./JS-FABigation.js"></script>
 
-function FABigation() {
-	window.FABigation = undefined
+var FABkey = {
+	"TSS": 100, // touchscreen sensitivity
+	"TPS": 100, // touchpad sensitivity
+	"Auto": 1, // automatic gestures
+	"Sticky": 0, // sticky scroll-to-top
+	"Hori": 1, // enable horizontal gestures
+	"Vert": 1 // enable vertical gestures
+}
+
+function FABigation() { window.FABigation = function() {}
+
+	var webx = (typeof browser !== "undefined" || typeof chrome !== "undefined")
+
+	function sync() { noIO()
+		function output(result) {
+			FABkey[Object.keys(result)[0]] = Number(result[Object.keys(result)])
+		}
+		for (var i in FABkey) {
+			if (typeof browser !== 'undefined') browser.storage.sync.get(i).then(output);
+			 else chrome.storage.sync.get(i,output)
+		}
+	}
+	if (webx) ((typeof browser !== 'undefined')?browser:chrome).storage.onChanged.addListener(sync)
+
+	function auto(F) {
+		if (auto.pause) return;
+		if (F==L) back();
+		if (F==R) forward();
+		if (F==T) reload();
+		if (F==B && !isScroll.on) home();
+		auto.pause=setTimeout(function(){auto.pause=0;},500)
+	}
 
 	function setIO(Fon,Foff,IO) {
 		if (Fon) {
-			if ((Fon==R || Fon==L)) (Fon).setAttribute("D",window.history.length);
+			if ((Fon==R || Fon==L)) {
+				if (!FABkey["Hori"]) return;
+				(Fon).setAttribute("D",window.history.length);
+			}
+			if ((Fon==T || Fon==B))
+				if (!FABkey["Vert"]) return;
 			if (Fon==B && atEnd()) (Fon).setAttribute("D", atTop())
+			if (Fon==B && FABkey["Sticky"]) return;
 			clearTimeout(Fon.T);
 			Fon.T = setTimeout(function(){(Fon).setAttribute("IO",0);(Fon).IO=0;}, 1500*IO);
 			(Fon).setAttribute("IO",IO); (Fon).IO = IO;
+			if (FABkey["Auto"] && IO==1) auto(Fon)
 		}
 		(Foff).setAttribute("IO",0); (Foff).IO = 0;
 	}
 
-	var BG = document.createElement("span"), L = document.createElement("div"), R = document.createElement("div");
-		L.className = R.className = "FABdo"; 
-		L.id = "LFAB"; setIO(0,L,0);
-		R.id = "RFAB"; setIO(0,R,0);
-	var B = document.createElement("div"), T = document.createElement("div");
-		B.className = T.className = "FABdo";
-		T.id = "TFAB"; setIO(0,T,0);
-		B.id = "BFAB"; setIO(0,B,0);
-	var oldDY = 0, oldCX = 0, Z = 91, TAr = [];
-	BG.appendChild(L); BG.appendChild(R); BG.appendChild(B); BG.appendChild(T);
+	var BG = document.createElement("span"), L = document.createElement("div"), R = document.createElement("div"), B = document.createElement("div"), T = document.createElement("div");
+		L.className = R.className = B.className = T.className = "FABdo", TAr = [];
+		L.id = "LFAB"; R.id = "RFAB"; T.id = "TFAB"; B.id = "BFAB";
+		BG.appendChild(L); BG.appendChild(R); BG.appendChild(B); BG.appendChild(T);
+	function noIO() {setIO(0,L,0);setIO(0,R,0);setIO(0,T,0);setIO(0,B,0);} noIO() 
+	if (webx) sync();
 
 	function back() {window.history.back()}
 	L.addEventListener("click", back, {passive: true});
 	function forward() {window.history.forward()}
 	R.addEventListener("click", forward, {passive: true});
-	function home() {window.scrollTo({top:0, behavior:"smooth"})} //(CSS.supports("-moz-hyphens","none")?"auto":"smooth")
+	function home() {window.scrollTo({top:0, behavior:(isScroll.on?"auto":"smooth")})}
 	B.addEventListener("click", home, {passive: true});
 	function reload() {location.reload();}
 	T.addEventListener("click", reload, {passive: true});
 
-	function atTop() {return +(window.pageYOffset < 3)}
+	function atTop(y) {return +(window.pageYOffset < (y||3))}
 	function atEnd() {return (window.innerHeight + window.scrollY + 3 >= ([document.body.scrollHeight, document.body.offsetHeight, document.scrollingElement.scrollHeight].sort(function(a,b){return b-a}))[0]);}
 
-	function wheel(e) {
-		if (!e.deltaY && oldCX==e.clientX) {
+	function sticky() {var IO = +!atTop(100); if (FABkey["Sticky"] && IO!=B.getAttribute("IO")) B.setAttribute("IO", IO)}
+	window.addEventListener("scroll", sticky, {passive: true})
+
+	function isScroll() {clearTimeout(isScroll.on); isScroll.on=setTimeout(function(){isScroll.on=0},10)}
+
+	function wheel(e) { isScroll();
+		var Z = 210 - FABkey["TPS"] || 91; 
+		if (!e.deltaY && wheel.oldCX==e.clientX) {
 		    if (e.deltaX>Z) setIO(R,L,1);
 				else
 			if (e.deltaX>Z/2) setIO(R,L,(R.IO==1?1:0.5))
@@ -47,25 +87,26 @@ function FABigation() {
 				else
 			if (e.deltaX<-Z/2) setIO(L,R,(L.IO==1?1:0.5))
 		}
-		oldCX = e.clientX
+		wheel.oldCX = e.clientX
 
-		if (atTop() && oldDY<-Z) {
+		if (atTop() && wheel.oldDY<-Z) {
 			if (e.deltaY<-Z) setIO(T,B,1)
 				else
 			if (e.deltaY<-Z/2) setIO(T,B,(T.IO==1?1:0.5))
 		}
 			else
-		if (atEnd() && oldDY>Z) {
+		if (atEnd() && wheel.oldDY>Z) {
 			if (e.deltaY>Z) setIO(B,T,1)
 				else
 			if (e.deltaY>Z/2) setIO(B,T,(B.IO==1?1:0.5))
 		}
-		oldDY = Math.round(e.deltaY/10)*10
+		wheel.oldDY = Math.round(e.deltaY/10)*10
 	}
 	window.addEventListener('wheel', wheel, {passive: true});
 
 	function TArO(N,n) {TAr[N] = TAr[0][n] - TAr[TAr.length-1][n]}
-	function Tmove(e) {
+	function Tmove(e) { isScroll();
+		var Z = 210 - FABkey["TSS"] || 91; 
 		TAr.push([e.changedTouches[0].pageX,e.changedTouches[0].pageY,e.changedTouches[0].clientX,e.changedTouches[0].clientY])
 		TAr.multi = (TAr.multi?true:(e.changedTouches[1]!=undefined))
 		TArO("PdX", 0); TArO("PdY", 1); TArO("CdX", 2); TArO("CdY", 3);
@@ -105,7 +146,7 @@ function FABigation() {
 	.FABdo {
 		all: initial;
 		--FABsize: calc(8vh + 4px);
-		z-index: 9999;
+		z-index: 10001;
 		cursor: pointer;
 		-webkit-tap-highlight-color: transparent;
 		user-select: none;
@@ -156,7 +197,7 @@ function FABigation() {
 	}
 
 /**/
-	html, body {overscroll-behavior-x: none;}
+	html, body {overscroll-behavior-x: none; overscroll-behavior-y: none;}
 
 	#LFAB, #RFAB {
 		top: 50%;
@@ -226,7 +267,3 @@ function FABigation() {
 }
 
 if (document.readyState != "loading") FABigation(); else window.addEventListener("load", FABigation);
-
-// if (CSS.supports("-moz-hyphens","none")){
-// 	function Bscroll(e) {if (atTop()) e.preventDefault();}
-// 	B.addEventListener("wheel", Bscroll, {passive: false});}
